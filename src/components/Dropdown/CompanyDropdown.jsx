bff6038a734f1621fa4bc9ed2324445310d55bdd
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-11-27 13:38:49 +0100

    Lagt till Dropdownmeny för att enkelt kunna välja företag och lista det företagets events

---
+++ src/components/Dropdown/CompanyDropdown.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+const CompanyDropdown = ({ onSelect, companies, selectedCompany }) => {
+    const dropdownRef = useRef(null);
+
+    const handleClick = (company) => {
+        dropdownRef.current.removeAttribute("open");
+        onSelect(company);
+    };
+
+    return (
+        <>
+            <details className="dropdown dropdown-bottom" ref={dropdownRef}>
+                <summary className="mb-1 btn">{selectedCompany ? `${selectedCompany.full_name} ▾`: "Choose Company"}</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-lg w-full">
+                    {companies?.map((company) => (
+                        <li key={company.id}>
+                            <a onClick={() => handleClick(company)}>{company.full_name}</a>
+                        </li>
+                    ))}
+                </ul>
+            </details>
+        </>
+    );
+};
+
+export default CompanyDropdown;
--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -2,30 +2,43 @@
 import { useState, useEffect } from "react";
 import ButtonStart from "../Button/ButtonStart";
 
-const EventList = () => {
-    const [companies, setCompanies] = useState([]);
+const EventList = ({ company, children }) => {
+    const [events, setEvents] = useState([]);
+    const [selectedCompany, setSelectedCompany] = useState(company);
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch("http://localhost:5030/companies");
-            const data = await response.json();
-            setCompanies(data);
+            try {
+                const response = await fetch(`http://localhost:5279/api/events/getcompanyevents/${selectedCompany.id}`);
+                const data = await response.json();
+                setEvents(data);
+            } catch (error) {
+                console.log(error);
+            }
         };
 
         fetchData();
-    }, []);
-    console.log(companies);
+    }, [selectedCompany]);
+
+    useEffect(() => {
+        setSelectedCompany(() => company);
+    });
+
+    useEffect(() => {
+        console.log(`${events}, ${selectedCompany}`);
+    }, [events, selectedCompany]);
+
+    console.log(company);
 
     return (
-        <div className="w-full flex justify-center py-8">
-            <ul className="grid md:grid-cols-3 sm:grid-cols-1 justify-center gap-8 w-fit">
-                {companies[0]?.events?.map((event) => (
-                    <li key={event.id} className="w-fit flex justify-center items-center">
-                        <div className="card bg-melon shadow-xl p-5 w-fit border rounded-xl border-pear">
-                            <div className="card-body">
-                                <p>{event.name ?? "Event name is not available"}</p>
-                                <ButtonStart text={"Open Quiz"} link="/quiz" />
-                            </div>
+        <div className="w-full flex flex-col gap-2 justify-center items-center py-8">
+            <h2>{children}</h2>
+            <ul className="grid md:grid-cols-3 sm:grid-cols-1 justify-center items-center w-fit gap-4">
+                {events?.map((event) => (
+                    <li key={event.id} className="flex justify-center items-center">
+                        <div className="card bg-melon shadow-xl px-5 py-8 gap-1 items-center justify-center border rounded-xl border-pear">
+                            <p>{event.name ?? "Event name is not available"}</p>
+                            <ButtonStart text={"Open Quiz"} link="/quiz" />
                         </div>
                     </li>
                 ))}
