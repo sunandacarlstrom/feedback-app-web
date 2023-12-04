bd334dc641d31534a14e62a4303a797f1d799f7f
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-12-04 13:43:29 +0100

    Justering av storlek på kort i EventList

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -35,8 +35,8 @@ const EventList = ({ company, children }) => {
             <h2>{children}</h2>
             <ul className="grid md:grid-cols-3 sm:grid-cols-1 justify-center items-center w-fit gap-4">
                 {events?.map((event) => (
-                    <li key={event.id} className="flex justify-center items-center">
-                        <div className="card bg-melon shadow-xl px-5 py-8 gap-1 items-center justify-center border rounded-xl border-pear">
+                    <li key={event.id} className="flex justify-center items-center text-center">
+                        <div className="card bg-melon shadow-xl px-10 py-8 gap-1 items-center justify-center border rounded-xl border-pear">
                             <p>{event.name ?? "Event name is not available"}</p>
                             <ButtonStart text={"Open Quiz"} link={`companyEvent/${event.id}`} />
                         </div>
