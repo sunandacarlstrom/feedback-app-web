6b19001acf2e95c2be4c7dff6ab1eaa5e42ddf36
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-12-28 12:39:10 +0100

    Spara formulärdata i en UseState

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,6 +1,22 @@
+"use client";
 import ButtonLogin from "@/components/Buttons/ButtonLogin";
+import { useEffect, useState } from "react";
 
 const LoginForm = () => {
+    const [form, setForm] = useState({ email: "", password: "" });
+
+    const onChange = (e) => {
+        setForm((prevForm) => {
+            const updatedForm = { ...prevForm };
+            updatedForm[e.target.id] = e.target.value;
+            return updatedForm;
+        });
+    };
+
+    useEffect(() => {
+        console.log(form);
+    }, [form]);
+
     return (
         <>
             <div className="card bg-digital-white shadow-xl p-4">
@@ -8,9 +24,25 @@ const LoginForm = () => {
                     <h2 className="card-title mb-4 text-center">Log in as Admin</h2>
                     <form id="login-form">
                         <div className="flex flex-col gap-4 justify-center items-center">
-                            <input type="text" placeholder="Username" name="username" id="login-username" className="border p-2 rounded-md w-full focus:outline-none focus:border-pear" />
-                            <input type="password" placeholder="Password" name="password" id="login-password" className="border p-2 rounded-md w-full focus:outline-none focus:border-pear" />
-                            <ButtonLogin link="/admin/companyEvent">Log in</ButtonLogin>
+                            <input
+                                type="text"
+                                value={form.email}
+                                onChange={onChange}
+                                placeholder="Email"
+                                name="email"
+                                id="email"
+                                className="border p-2 rounded-md w-full focus:outline-none focus:border-pear"
+                            />
+                            <input
+                                type="password"
+                                value={form.password}
+                                onChange={onChange}
+                                placeholder="Password"
+                                name="password"
+                                id="password"
+                                className="border p-2 rounded-md w-full focus:outline-none focus:border-pear"
+                            />
+                            <ButtonLogin>Log in</ButtonLogin>
                         </div>
                     </form>
                 </div>
