d6afbc2dadd224da48d671c76919b3ae111483ac
Sunanda Carlström <carlstrom-3@hotmail.com>
2023-12-28 13:08:12 +0100

    Lagt till kontroll av email och lösenord samt felmeddelande i form av Toast

--- src/components/Forms/LoginForm/LoginForm.jsx
+++ src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,16 +1,27 @@
 "use client";
 import ButtonLogin from "@/components/Buttons/ButtonLogin";
 import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
 const LoginForm = () => {
     const [form, setForm] = useState({ email: "", password: "" });
 
-    const onChange = (e) => {
+    const handleInput = (e) => {
         setForm((prevForm) => {
             const updatedForm = { ...prevForm };
             updatedForm[e.target.id] = e.target.value;
             return updatedForm;
         });
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if (form.email === "" || form.password === "") {
+            toast.error("Please fill the login form!", { theme: "colored" });
+            return;
+        }
+        console.log("yest");
     };
 
     useEffect(() => {
@@ -22,12 +33,12 @@ const LoginForm = () => {
             <div className="card bg-digital-white shadow-xl p-4">
                 <div className="card-body">
                     <h2 className="card-title mb-4 text-center">Log in as Admin</h2>
-                    <form id="login-form">
+                    <form id="login-form" onSubmit={handleLogin}>
                         <div className="flex flex-col gap-4 justify-center items-center">
                             <input
                                 type="text"
                                 value={form.email}
-                                onChange={onChange}
+                                onChange={handleInput}
                                 placeholder="Email"
                                 name="email"
                                 id="email"
@@ -36,7 +47,7 @@ const LoginForm = () => {
                             <input
                                 type="password"
                                 value={form.password}
-                                onChange={onChange}
+                                onChange={handleInput}
                                 placeholder="Password"
                                 name="password"
                                 id="password"
@@ -47,6 +58,7 @@ const LoginForm = () => {
                     </form>
                 </div>
             </div>
+            <ToastContainer />
         </>
     );
 };
